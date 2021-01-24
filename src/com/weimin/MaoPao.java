5253a77389fbc8e8ad3f5345cff4c4e89695242d
weimin <2561394933@qq.com>
2021-01-24 16:04:36 +0800

    sort

--- src/com/weimin/MaoPao.java
+++ src/com/weimin/MaoPao.java
@@ -1,11 +1,73 @@
 package com.weimin;
 
+import java.util.Arrays;
+
 /**
  * 冒泡排序
- *
+ * o(n^2)
  */
 public class MaoPao {
     public static void main(String[] args) {
+        int[] array = {3, 9, -1, 10, -2};
+        sort(array);
+        System.out.println(Arrays.toString(array));
+    }
 
+    public static void sort(int[] array) {
+        // 这个change是优化
+        boolean change = false;
+        int temp = 0;
+        for (int i = 0; i < array.length - 1; i++) {
+            for (int j = 0; j < array.length - 1 - i; j++) {
+                if (array[j] > array[j + 1]) {
+                    change = true;
+                    temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+
+            if(change){
+                // 重置
+                change = false;
+            }else {
+                break;
+            }
+        }
+    }
+
+    public static void sortTest(int[] array) {
+        int temp = 0;
+        for (int j = 0; j < array.length - 1; j++) {
+            if (array[j] > array[j + 1]) {
+                temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+            }
+        }
+
+        for (int j = 0; j < array.length - 1 - 1; j++) {
+            if (array[j] > array[j + 1]) {
+                temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+            }
+        }
+
+        for (int j = 0; j < array.length - 1 - 2; j++) {
+            if (array[j] > array[j + 1]) {
+                temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+            }
+        }
+
+        for (int j = 0; j < array.length - 1 - 3; j++) {
+            if (array[j] > array[j + 1]) {
+                temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+            }
+        }
     }
 }
