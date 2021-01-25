82a4ce949abd1b82f3401b4f235c311a33b0c1be
weimin <2561394933@qq.com>
2021-01-25 13:52:36 +0800

    sort

---
+++ src/com/weimin/Insert.java
@@ -0,0 +1,9 @@
+package com.weimin;
+
+import java.util.UUID;
+
+public class Insert {
+    public static void main(String[] args) {
+        System.out.println(UUID.randomUUID().toString());
+    }
+}
